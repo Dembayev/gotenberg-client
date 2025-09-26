61985f763669291dce8a777c4b3d2d4f671c39e3
sauran <sbaitarakhov@outlook.com>
2025-09-26 16:38:11 +0500

    Implement webhook handler and server setup for HTML to PDF conversion

--- example/cmd/html2pdf_webhook/main.go
+++ example/cmd/html2pdf_webhook/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"fmt"
-	"io"
 	"log"
 	"log/slog"
 	"net/http"
@@ -20,6 +18,8 @@ import (
 )
 
 func main() {
+	srv := StartServer(":28080")
+
 	gotenbergURL := `http://localhost:3000`
 
 	httpClient := &http.Client{
@@ -27,18 +27,6 @@ func main() {
 	}
 
 	client := gotenberg.NewClient(httpClient, gotenbergURL)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/success", webhookHandler("success"))
-	mux.HandleFunc("/error", webhookHandler("error"))
-
-	srv := &http.Server{Addr: ":28080", Handler: mux}
-	go func() {
-		slog.Info("starting local webhook server", "addr", srv.Addr)
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("webhook server error: %v", err)
-		}
-	}()
 
 	data := model.InvoiceData
 	html := bytes.NewBuffer(nil)
@@ -79,39 +67,5 @@ func main() {
 	defer cancel()
 	if err := srv.Shutdown(shutdownCtx); err != nil {
 		slog.Info("error shutting down server", "err", err)
-	}
-}
-
-func webhookHandler(name string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		outFile, err := os.Create("invoice_async.pdf")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer outFile.Close()
-
-		n, err := io.Copy(outFile, r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer r.Body.Close()
-
-		slog.Info(fmt.Sprintf("webhook %s received", name),
-			"method", r.Method,
-			"path", r.URL.Path,
-			"gotenberg-trace", r.Header.Get("Gotenberg-Trace"),
-			"authorization", r.Header.Get("Authorization"),
-			"x-custom-header", r.Header.Get("X-Custom-Header"),
-			"body lenth", n,
-		)
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(`{"status":"ok"}`))
 	}
 }
